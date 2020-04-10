029b40f816e685779b71392073c3ff5651222416
Kathleen Bongco <kathleen.bongco@gmail.com>
2020-04-10 17:20:15 -0400

    editin routing/database code

--- samplemanage.py
+++ samplemanage.py
@@ -8,24 +8,6 @@ from tables import AllSamples
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sql:///{}".format(os.path.join(project_dir, "sample.db"))
-SQLALCHEMY_TRACK_MODIFICATIONS = False 
-
-
-app = Flask(__name__, template_folder = 'templates')
-app.config["SQLALCHEMY_DATBASE_URI"] = database_file
-db = SQLAlchemy(app)
-
-class samples(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    lotnumber = db.Column(db.String(100), unique = True, nullable = False)
-    samplename = db.Column(db.String(100), unique = False, nullable = False)
-    name = db.Column(String(100), unique = False, nullable = False)
-    submissiondate = db.Column(db.String(100), nullable = False)
-    tests = db.Column(db.String(150), nullable = False)
-
-
 
 
 @app.route('/')
@@ -34,9 +16,8 @@ def home():
 
 @app.route('/view')
 def samples():
-    if request.form:
-        viewsamples = session.query(samples).all()
-        return render_template('view.html', viewsamples = viewsamples)
+    viewsamples = samples.query.all()
+    return render_template('view.html', viewsamples = viewsamples)
 
 @app.route('/new', methods = ['GET', 'POST'])
 def new():
